02d38eee60af30b1cc59e7678167e35d4272710a
Brock Luker <brockluker@gmail.com>
2016-01-29 12:38:41 -0700

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | CircleJerk"
+    assert_select "title", "CircleJerk"
   end
 
   test "should get help" do
